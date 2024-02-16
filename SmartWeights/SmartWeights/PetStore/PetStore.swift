cfdba3ab53699d4824d942b8d55e8c497c04b4ce
Jonathan Stanczak <jtstanczak7@verizon.net>
2024-02-16 10:40:28 -0500

    Initial setup/creating files. Also testing push and pull functions in Swift

---
+++ SmartWeights/SmartWeights/PetStore/PetStore.swift
@@ -0,0 +1,26 @@
+//
+//  PetStore.swift
+//  SmartWeights
+//
+//  Created by Jonathan Stanczak on 2/16/24.
+//
+
+// Initial setup
+
+import Foundation
+import SwiftUI
+
+struct PetStore: View {
+    var body: some View {
+            Text("Pet Store")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+        Spacer()
+        }
+    
+}
+
+#Preview {
+    PetStore()
+}
+
