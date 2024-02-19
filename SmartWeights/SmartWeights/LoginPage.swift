9d7565b96ab0d080c94e0512e5867c03fbf79c0f
parchea <parodamchea@gmail.com>
2024-02-19 10:30:06 -0500

    Login Page

---
+++ SmartWeights/SmartWeights/LoginPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPage.swift
+//  SmartWeights
+//
+//  Created by par chea on 2/19/24.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginPage()
+}
