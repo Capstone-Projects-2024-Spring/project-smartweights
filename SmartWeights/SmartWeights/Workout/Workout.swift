8f8deb08abc91edc089862e4adf6c8a86dfef8cb
Tu <tun42092@temple.edu>
2024-02-19 21:02:05 -0500

    created file for workout page

---
+++ SmartWeights/SmartWeights/Workout/Workout.swift
@@ -0,0 +1,18 @@
+//
+//  Workout.swift
+//  SmartWeights
+//
+//  Created by Tu Ha on 2/19/24.
+//
+
+import SwiftUI
+
+struct Workout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Workout()
+}
