55ada601511eb011f082659ee001f6bdf7a0c28d
dillon09776 <tup09776@temple.edu>
2024-02-23 10:11:23 -0500

    Removed imageFill var and placed images in VStack with a spacer to position at the bottom of the screen.

--- SmartWeights/SmartWeights/Navbar.swift
+++ SmartWeights/SmartWeights/Navbar.swift
@@ -27,36 +27,35 @@ extension Color {
 
 struct Navbar: View {
     @Binding var selectedTab: Tab // Paired with @State var in ContentView
-    private var fillImage: String {
-        selectedTab.rawValue //+ ".fill"
-    }
     
     var body: some View {
-        Spacer()
-        HStack {
+        // Need to add some frame to VStack
+        VStack {
             Spacer()
-            ForEach(Tab.allCases, id: \.rawValue) { tab in
+            HStack {
                 Spacer()
-                Image(systemName: selectedTab == tab ? fillImage : tab.rawValue)
-                    .foregroundStyle(selectedTab == tab ? Color.hex212121 : Color.hexA6A6A6)
-                    .font(.system(size: 22))
-                    .scaleEffect(selectedTab == tab ? 1.25 : 1.0)
-                    .offset(y: selectedTab == tab ? -30 : 10) // Anyway to not hard-code values?
-                    .background(Circle().opacity(selectedTab == tab ? 1.0 : 0.0)
-                        .offset(y: selectedTab == tab ? -30: 0)
-                        .frame(width: 65, height: 65)
-                        .foregroundStyle(Color.africanViolet))
-                    .onTapGesture {
-                        withAnimation(.easeIn(duration: 0.1)) {
-                            selectedTab = tab
+                ForEach(Tab.allCases, id: \.rawValue) { tab in
+                    Spacer()
+                    Image(systemName: tab.rawValue)
+                        .foregroundStyle(selectedTab == tab ? Color.hex212121 : Color.hexA6A6A6)
+                        .font(.system(size: 22))
+                        .scaleEffect(selectedTab == tab ? 1.25 : 1.0)
+                        .offset(y: selectedTab == tab ? -40 : 0) // Anyway to not hard-code values?
+                        .background(Circle().opacity(selectedTab == tab ? 1.0 : 0.0)
+                            .offset(y: selectedTab == tab ? -40: 0)
+                            .frame(width: 65, height: 65)
+                            .foregroundStyle(Color.africanViolet))
+                        .onTapGesture {
+                            withAnimation(.easeIn(duration: 0.1)) {
+                                selectedTab = tab
+                            }
                         }
-                    }
-                Spacer()
+                    Spacer()
+                }
             }
-            Spacer()
+            .frame(width: nil, height: 80)
+            .background(Color.hex212121)
         }
-        .frame(width: nil, height: 60)
-        .background(Color.hex212121)
     }
 }
 
