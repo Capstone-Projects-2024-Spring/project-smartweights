04a213ccda0029acfce4715ffa2d2e589f42dc02
dillon09776 <tup09776@temple.edu>
2024-02-25 17:16:33 -0500

    Added rough frame for homepage

--- SmartWeights/SmartWeights/Homepage.swift
+++ SmartWeights/SmartWeights/Homepage.swift
@@ -7,15 +7,61 @@
  
 import SwiftUI
 
+var currentWorkout = "Dumbbell Press"
+
 struct Homepage: View {
 
     var body: some View {
-        Text("Homepage")
-            .foregroundStyle(.white)
-            .frame(maxWidth: .infinity, maxHeight: .infinity) // Expand text container for background visibility
-            .background(Color.black) // Apply color as background
-            .edgesIgnoringSafeArea(.top) // Ignore the top safe area
-            .edgesIgnoringSafeArea(.bottom) // Ignore the bottom safe area
+        VStack {
+            // Welcome Message
+            HStack {
+                Text("Welcome Back")
+            }
+            
+            // Start Workout Button
+            ZStack {
+                HStack {
+                    VStack {
+                        HStack {
+                            Text("Start Workout")
+                            Image(systemName: "arrow.right")
+                        }
+                        Text(currentWorkout)
+                    }
+                    Image(systemName: "photo")
+                }
+            }
+            
+            // Navigation Carousel
+            VStack {
+                Text("Button Carousel")
+                HStack {
+                    let count = 1...4
+                    ForEach(count, id: \.self) { _ in
+                        Image(systemName: "photo")
+                            .background(Circle())
+                    }
+                }
+            }
+            
+            // Video Carousel
+            VStack {
+                HStack {
+                    Text("Videos")
+                    Image(systemName: "arrow.right")
+                }
+                HStack {
+                    let count = 1...4
+                    ForEach(count, id: \.self) { number in
+                        VStack {
+                            Image(systemName: "photo")
+                            Text("Video \(number)")
+                            Text("Video Description")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
