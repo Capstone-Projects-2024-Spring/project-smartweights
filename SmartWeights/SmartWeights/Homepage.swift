5f08f4e9129c3095d157e1df0388d75e6a766c6f
dillon09776 <tup09776@temple.edu>
2024-02-25 17:24:55 -0500

    Added spacing

--- SmartWeights/SmartWeights/Homepage.swift
+++ SmartWeights/SmartWeights/Homepage.swift
@@ -13,11 +13,15 @@ struct Homepage: View {
 
     var body: some View {
         VStack {
+            Spacer()
             // Welcome Message
             HStack {
-                Text("Welcome Back")
+                Text("Welcome")
+                    .padding()
+                Spacer()
             }
             
+            Spacer()
             // Start Workout Button
             ZStack {
                 HStack {
@@ -28,39 +32,55 @@ struct Homepage: View {
                         }
                         Text(currentWorkout)
                     }
+                    .padding()
+                    Spacer()
                     Image(systemName: "photo")
+                        .padding()
                 }
             }
             
+            Spacer()
             // Navigation Carousel
             VStack {
                 Text("Button Carousel")
-                HStack {
-                    let count = 1...4
-                    ForEach(count, id: \.self) { _ in
-                        Image(systemName: "photo")
-                            .background(Circle())
-                    }
-                }
-            }
-            
-            // Video Carousel
-            VStack {
-                HStack {
-                    Text("Videos")
-                    Image(systemName: "arrow.right")
-                }
-                HStack {
-                    let count = 1...4
-                    ForEach(count, id: \.self) { number in
-                        VStack {
+                ScrollView (.horizontal) {
+                    HStack {
+                        let count = 1...4
+                        ForEach(count, id: \.self) { _ in
                             Image(systemName: "photo")
-                            Text("Video \(number)")
-                            Text("Video Description")
+                                .background(Circle())
+                                .padding()
                         }
                     }
                 }
             }
+            
+            Spacer()
+            // Video Carousel
+            VStack {
+                HStack {
+                    Text("Videos")
+                        .padding()
+                    Spacer()
+                    Image(systemName: "arrow.right")
+                        .padding()
+                }
+                ScrollView (.horizontal) {
+                    HStack {
+                        let count = 1...4
+                        ForEach(count, id: \.self) { number in
+                            VStack {
+                                Image(systemName: "photo")
+                                Text("Video \(number)")
+                                Text("Video Description")
+                            }
+                            .padding()
+                        }
+                    }
+                }
+            }
+            
+            Spacer()
         }
     }
 }
