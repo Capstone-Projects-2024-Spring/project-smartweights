5eb507758644a08db01aeb035f172df6b434ec72
parchea <parodamchea@gmail.com>
2024-02-25 19:34:06 -0500

    Navbar, NavController, and Homepage - All work is by Dillion

---
+++ SmartWeights/SmartWeights/Navbar/NavController.swift
@@ -0,0 +1,45 @@
+//
+//  BarNavigation.swift
+//  SmartWeights
+//
+//  Created by par chea on 2/21/24.
+//
+
+import SwiftUI
+
+/// A navigation controller that manages the navigation between different views in the SmartWeights app.
+struct NavController: View {
+    @State private var selectedTab: Tab = .house
+
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                // Use the selectedTab to determine which view to show
+                switch selectedTab {
+                case .house:
+                    Homepage().frame(maxWidth: .infinity, maxHeight: .infinity)
+                case .message:
+                    PetStore().frame(maxWidth: .infinity, maxHeight: .infinity)
+                case .person:
+                    PostWorkout().frame(maxWidth: .infinity, maxHeight: .infinity)
+                case .leaf:
+                    Calendar().frame(maxWidth: .infinity, maxHeight: .infinity)
+                case .gearshape:
+                    WorkoutGraph().frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+
+                // The navbar at the bottom, ensuring it doesn't overlap the content
+                //Navbar(selectedTab: $selectedTab)
+                    //.frame(height: 50) // Adjust this height as needed
+            }
+            VStack {
+                Spacer()
+                Navbar(selectedTab: $selectedTab)
+            }
+        } // Ensure it fits full screen including the bottom edge
+    }
+}
+
+#Preview {
+    NavController()
+}
