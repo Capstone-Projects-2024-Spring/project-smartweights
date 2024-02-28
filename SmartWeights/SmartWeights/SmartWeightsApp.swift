ee01be3a8e248fa94b2472041642cc90d1cb35c1
DanielEap <danieliseap@gmail.com>
2024-02-28 13:48:20 -0500

    got rid of all things firebase

--- SmartWeights/SmartWeights/SmartWeightsApp.swift
+++ SmartWeights/SmartWeights/SmartWeightsApp.swift
@@ -6,21 +6,10 @@
 //
 
 import SwiftUI
-import FirebaseCore
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
 @main
 struct SmartWeightsApp: App {
     let persistenceController = PersistenceController.shared
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
