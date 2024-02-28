fb408da4b1cc0ec5cf31c457e01c36e27c1c194a
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2024-02-27 19:15:41 -0500

    Created MorePageView.swift and began working on the More... page's UI

--- SmartWeights/SmartWeights/More/MorePageView.swift
+++ SmartWeights/SmartWeights/More/MorePageView.swift
@@ -8,79 +8,106 @@
 import SwiftUI
 
 struct MorePageView: View {
+    let profile = Profile(firstName: "First", lastName: "Last", level: 1)
+    let achievements = [
+        Achievement(title: "Achievement 1", description: "", img: "trophy.circle"),
+        Achievement(title: "Achievement 2", description: "", img: "trophy.circle"),
+        Achievement(title: "Achievement 3", description: "", img: "trophy.circle"),
+        Achievement(title: "Achievement 4", description: "", img: "trophy.circle"),
+        Achievement(title: "Achievement 5", description: "", img: "trophy.circle"),
+        Achievement(title: "Achievement 6", description: "", img: "trophy.circle")
+    ]
+    
     var body: some View {
-        NavigationView {
+        NavigationStack {
             VStack {
-                HStack {
-                    Spacer()
-                    Image(systemName: "gearshape")
-                        .padding(
-                            [.trailing],
-                            30
-                        )
-                }
-                Text("Username")
+                Text("\(profile.firstName) \(profile.lastName)")
                 Image(systemName: "person.circle")
                     .resizable()
                     .frame(
                         width: 100,
                         height: 100
                     )
-                Text("Lv. 1")
+                Text("Lv. \(profile.level)")
                 ProgressView(value: 0.5)
                     .progressViewStyle(LinearProgressViewStyle())
                     .frame(
-                        width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/
+                        width: 100
                     )
-                Spacer()
+                Divider()
                 VStack {
                     Text("Achievements")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    HStack {
-                        VStack {
-                            Image(systemName: "trophy.circle")
-                                .resizable()
-                                .frame(
-                                    width: 75,
-                                    height: 75
-                                )
-                            Text("Achievement Title")
-                                .font(.subheadline)
-                        }
-                        VStack {
-                            Image(systemName: "trophy.circle")
-                                .resizable()
-                                .frame(
-                                    width: 75,
-                                    height: 75
-                                )
-                            Text("Achievement Title")
-                                .font(.subheadline)
-                        }
-                        VStack {
-                            Image(systemName: "trophy.circle")
-                                .resizable()
-                                .frame(
-                                    width: 75,
-                                    height: 75
-                                )
-                            Text("Achievement Title")
-                                .font(.subheadline)
+                        .font(.title3)
+                        .fontWeight(.medium)
+                    ScrollView(.horizontal) {
+                        HStack {
+                            Spacer()
+                            ForEach(achievements) { achievement in
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                        .fill(Color.green)
+                                        .frame(width: 120, height: 140)
+                                    VStack(spacing: 20) {
+                                        Image(systemName: achievement.img)
+                                            .resizable()
+                                            .frame(
+                                                width: 50,
+                                                height: 50
+                                            )
+                                        VStack {
+                                            ForEach(achievement.title.split(separator: " "), id: \.self) { word in
+                                                            Text(String(word))
+                                                                .font(.caption)
+                                                        }
+                                        }
+                                    }
+                                }
+                            }
+                            Spacer()
                         }
                     }
-                    Spacer()
-                    Image(systemName: "dog")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(
-                            width: 300,
-                            height: 300
-                        )
-                    Spacer()
+                }
+                Divider()
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(
+                        width: 300,
+                        height: 300
+                    )
+                    .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                Spacer()
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Screenshot", systemImage: "camera") {
+                        
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Settings", systemImage: "gearshape") {
+                        
+                    }
                 }
             }
         }
     }
+}
+
+struct Profile: Identifiable {
+    var id = UUID()
+    var firstName: String
+    var lastName: String
+    var level: Int
+}
+
+struct Achievement: Identifiable {
+    var id = UUID()
+    var title: String
+    var description: String
+    var img: String    
 }
 
 #Preview {
