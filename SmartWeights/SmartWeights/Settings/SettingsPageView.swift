114dfed647b3716f26ab161394814919154289e3
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2024-03-01 03:44:45 -0500

    Created SettingsPageView.swift

---
+++ SmartWeights/SmartWeights/Settings/SettingsPageView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsPageView.swift
+//  SmartWeights
+//
+//  Created by Timothy Bui on 2/29/24.
+//
+
+import SwiftUI
+
+struct SettingsPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsPageView()
+}
