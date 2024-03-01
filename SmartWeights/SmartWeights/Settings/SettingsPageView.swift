359c1f9fb9f1bcf2d518c5754e0196da22347c6a
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2024-03-01 05:11:40 -0500

    Started working on UI for settings page

--- SmartWeights/SmartWeights/Settings/SettingsPageView.swift
+++ SmartWeights/SmartWeights/Settings/SettingsPageView.swift
@@ -8,8 +8,81 @@
 import SwiftUI
 
 struct SettingsPageView: View {
+    @State private var notificationsEnabled = false
+    @State private var darkModeEnabled = false
+    @State private var useSystemSettingsEnabled = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section(header: Text("Profile")) {
+                    Group {
+                        HStack {
+                            Spacer()
+                            VStack {
+                                Spacer()
+                                Image(systemName: "person.circle")
+                                    .resizable()
+                                    .frame(
+                                        width: 100,
+                                        height: 100
+                                    )
+                                Text("First Last")
+                                Button("Edit Profile") {
+                                    
+                                }
+                                .font(.footnote)
+                                .foregroundStyle(Color(Color.white))
+                                .padding()
+                                .background(Color.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                                Spacer()
+                            }
+                            Spacer()
+                        }
+                    }
+                }
+                Section(
+                    header: Text("Preferences"),
+                    footer: Text("System settings will override dark mode and use the current device's theme")
+                ) {
+                    HStack {
+                        Image(systemName: "globe")
+                        Text("Language")
+                    }
+                    HStack {
+                        Image(systemName: "bell")
+                        Toggle(isOn: $notificationsEnabled, label: {
+                            Text("Notifications")
+                        })
+                    }
+                    HStack {
+                        Image(systemName: "moon")
+                        Toggle(isOn: $darkModeEnabled, label: {
+                            Text("Dark Mode")
+                        })
+                    }
+                    HStack {
+                        Image(systemName: "gearshape")
+                        Toggle(isOn: $useSystemSettingsEnabled, label: {
+                            Text("Use System Settings")
+                        })
+                    }
+                }
+                Button("Logout", systemImage: "rectangle.portrait.and.arrow.right") {
+                    // Implement logout button functionality
+                }
+            }
+            .navigationTitle("Settings")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Back", systemImage: "arrowshape.backward.fill") {
+                        // Implement back button functionality
+                    }
+                }
+            }
+        }
     }
 }
 
