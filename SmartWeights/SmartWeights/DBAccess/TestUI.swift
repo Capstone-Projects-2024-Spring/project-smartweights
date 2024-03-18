4918a708c7647ddd57038646792fc9e2ceccd864
DanielEap <danieliseap@gmail.com>
2024-03-18 10:17:53 -0400

    quick comment

--- SmartWeights/SmartWeights/DBAccess/TestUI.swift
+++ SmartWeights/SmartWeights/DBAccess/TestUI.swift
@@ -1,42 +1,42 @@
+////
+////  TestUI.swift
+////  SmartWeights
+////
+////  Created by Daniel Eap on 3/15/24.
+////
 //
-//  TestUI.swift
-//  SmartWeights
+//import SwiftUI
 //
-//  Created by Daniel Eap on 3/15/24.
+//struct OnboardingView: View {
+//    @StateObject private var petViewModel = PetViewModel()
+//    @StateObject private var viewModel = OnboardingViewModel()
+//    @State private var accountStatusAlertShown = false
+//    @Environment(\.dismiss) var dismiss
 //
-
-import SwiftUI
-
-struct OnboardingView: View {
-    @StateObject private var petViewModel = PetViewModel()
-    @StateObject private var viewModel = OnboardingViewModel()
-    @State private var accountStatusAlertShown = false
-    @Environment(\.dismiss) var dismiss
-
-    var body: some View {
-        Button("startUsingApp") {
-            if viewModel.accountStatus != .available {
-                accountStatusAlertShown = true
-            } else {
-                // Show popup with user credentials
-                Task {
-                    await petViewModel.save()
-                    dismiss()
-                }
-                
-            }
-        }
-        .alert("iCloudAccountDisabled", isPresented: $accountStatusAlertShown) {
-            Button("cancel", role: .cancel, action: {})
-        }
-        .task {
-            await viewModel.fetchAccountStatus()
-        }
-        
-        
-    }
-}
-
-#Preview {
-    OnboardingView()
-}
+//    var body: some View {
+//        Button("startUsingApp") {
+//            if viewModel.accountStatus != .available {
+//                accountStatusAlertShown = true
+//            } else {
+//                // Show popup with user credentials
+//                Task {
+//                    await petViewModel.save()
+//                    dismiss()
+//                }
+//                
+//            }
+//        }
+//        .alert("iCloudAccountDisabled", isPresented: $accountStatusAlertShown) {
+//            Button("cancel", role: .cancel, action: {})
+//        }
+//        .task {
+//            await viewModel.fetchAccountStatus()
+//        }
+//        
+//        
+//    }
+//}
+//
+//#Preview {
+//    OnboardingView()
+//}
