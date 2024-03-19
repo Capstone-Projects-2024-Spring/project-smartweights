c6925ce63fa98d06f29798c32f61598bfc11d3dd
Tu <tun42092@temple.edu>
2024-03-18 20:43:53 -0400

    can read pico temperature via bluetooth

--- SmartWeights/SmartWeights/SmartWeightsApp.swift
+++ SmartWeights/SmartWeights/SmartWeightsApp.swift
@@ -12,7 +12,7 @@ struct SmartWeightsApp: App {
     let persistenceController = PersistenceController.shared
     var body: some Scene {
         WindowGroup {
-            BLEview()
+            bluetoothView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
