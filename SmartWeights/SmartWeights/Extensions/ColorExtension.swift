c34d6842d60d59c139325fa848ac3e96057159d6
dillon09776 <tup09776@temple.edu>
2024-03-23 20:55:25 -0400

    Moved declaration of custom colors to a new file.

---
+++ SmartWeights/SmartWeights/Extensions/ColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ColorExtension.swift
+//  SmartWeights
+//
+//  Created by Dillon Shi on 3/23/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let africanViolet = Color(red: 178/255, green: 132/255, blue: 190/255)
+    static let hexA6A6A6 = Color(red: 166/255, green: 166/255, blue: 166/255)
+    static let hex212121 = Color(red: 33/255, green: 33/255, blue: 33/255)
+    static let hex121212 = Color(red: 18/255, green: 18/255, blue: 18/255)
+}
--- SmartWeights/SmartWeights/Navbar/Navbar.swift
+++ SmartWeights/SmartWeights/Navbar/Navbar.swift
@@ -18,14 +18,6 @@ enum Tab: String, CaseIterable {
     case gearshape
 }
 
-// Custom colors
-extension Color {
-    static let africanViolet = Color(red: 178/255, green: 132/255, blue: 190/255)
-    static let hexA6A6A6 = Color(red: 166/255, green: 166/255, blue: 166/255)
-    static let hex212121 = Color(red: 33/255, green: 33/255, blue: 33/255)
-    static let hex121212 = Color(red: 18/255, green: 18/255, blue: 18/255)
-}
-
 /// A custom navigation bar view.
 struct Navbar: View {
     @Binding var selectedTab: Tab // Paired with @State var in ContentView
