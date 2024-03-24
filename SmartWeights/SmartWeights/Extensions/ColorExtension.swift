a160f852aa21a5a3a24a2a0b8a3f365782da550e
dillon09776 <tup09776@temple.edu>
2024-03-23 21:00:53 -0400

    Moved Color extension in Homepage to ColorExtension file. Renamed colors for better reference and understanding.

--- SmartWeights/SmartWeights/Extensions/ColorExtension.swift
+++ SmartWeights/SmartWeights/Extensions/ColorExtension.swift
@@ -12,4 +12,6 @@ extension Color {
     static let stoneGray = Color(red: 166/255, green: 166/255, blue: 166/255)
     static let charcoalGray = Color(red: 33/255, green: 33/255, blue: 33/255)
     static let midnightBlack = Color(red: 18/255, green: 18/255, blue: 18/255)
+    static let darkGray = Color(red: 46/255, green: 46/255, blue: 46/255)
+    static let lightGray = Color(red: 150/255, green: 150/255, blue: 150/255)
 }
