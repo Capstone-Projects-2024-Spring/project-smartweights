ac1f82a9e0e4a4690a59c5bc0c776aa4bb8719a3
DanielEap <danieliseap@gmail.com>
2024-03-24 23:35:40 -0400

    swap out load page

--- SmartWeights/SmartWeights/SmartWeightsApp.swift
+++ SmartWeights/SmartWeights/SmartWeightsApp.swift
@@ -12,10 +12,9 @@ struct SmartWeightsApp: App {
     let persistenceController = PersistenceController.shared
     var body: some Scene {
         WindowGroup {
-//            LoginView()
-//               .environment(\.managedObjectContext, persistenceController.container.viewContext)
-//            VoiceView()
-            WorkoutMainPage()
+            LoginView()
+               .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//            
         }
     }
 }
