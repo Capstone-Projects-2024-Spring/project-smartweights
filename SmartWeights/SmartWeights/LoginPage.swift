99e04eae4ab9f561df70f09d04a2af9d1b5732d5
Adam Ra <adamra97@gmail.com>
2024-04-01 11:43:33 -0400

    Included Login Caching so users don't need to constantly relogin

--- SmartWeights/SmartWeights/LoginPage.swift
+++ SmartWeights/SmartWeights/LoginPage.swift
@@ -26,63 +26,69 @@ struct LoginView: View {
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
-                Spacer()
-                
-                // App title
-                Text("SmartWeights")
-                    .font(.system(size: 55))
-                    .foregroundStyle(.white)
-                    .italic()
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding(.vertical, 20)
-                
-                // Logo with rounded corners
-                Image("logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 450, height: 350) // Adjust the size as needed
-                    .cornerRadius(50) // Rounded corners
-                    .shadow(radius: 10) // Add shadow
-                    .padding(.bottom, 20)
-                
-                // Sign In with Apple Button
-                SignInWithAppleButton(.continue) { request in
-                    // Configuration for the request here
-                    request.requestedScopes = [.fullName, .email]
-                } onCompletion: { result in
-                    switch result {
-                    case .success(let auth):
-                        if let credential = auth.credential as? ASAuthorizationAppleIDCredential {
-                            self.email = credential.email ?? ""
-                            self.firstName = credential.fullName?.givenName ?? ""
-                            self.lastName = credential.fullName?.familyName ?? ""
-                            self.userID = credential.user
-                            self.isLoggedIn = true
+                if userID.isEmpty{
+                    Spacer()
+                    
+                    // App title
+                    Text("SmartWeights")
+                        .font(.system(size: 55))
+                        .foregroundStyle(.white)
+                        .italic()
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.vertical, 20)
+                    
+                    // Logo with rounded corners
+                    Image("logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 450, height: 350) // Adjust the size as needed
+                        .cornerRadius(50) // Rounded corners
+                        .shadow(radius: 10) // Add shadow
+                        .padding(.bottom, 20)
+                    
+                    // Sign In with Apple Button
+                    SignInWithAppleButton(.continue) { request in
+                        // Configuration for the request here
+                        request.requestedScopes = [.fullName, .email]
+                    } onCompletion: { result in
+                        switch result {
+                        case .success(let auth):
+                            if let credential = auth.credential as? ASAuthorizationAppleIDCredential {
+                                self.email = credential.email ?? ""
+                                self.firstName = credential.fullName?.givenName ?? ""
+                                self.lastName = credential.fullName?.familyName ?? ""
+                                self.userID = credential.user
+                                self.isLoggedIn = true
+                            }
+                        case .failure(let error):
+                            print(error)
+                            alertMessage = "Sign in failed."
+                            showingAlert = true
                         }
-                    case .failure(let error):
-                        print(error)
-                        alertMessage = "Sign in failed."
-                        showingAlert = true
                     }
+                    .signInWithAppleButtonStyle(colorScheme == .dark ? .white : .black)
+                    .frame(width: 280, height: 45)
+                    .shadow(radius: 5)
+                    .padding()
+                    .alert(isPresented: $showingAlert) {
+                        Alert(title: Text("Sign In"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                    }
+                    Spacer()
                 }
-                .signInWithAppleButtonStyle(colorScheme == .dark ? .white : .black)
-                .frame(width: 280, height: 45)
-                .shadow(radius: 5)
-                .padding()
-                .alert(isPresented: $showingAlert) {
-                    Alert(title: Text("Sign In"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                else{
+                    //signed in
+                    TabBar()
                 }
-                Spacer()
+                    
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        // Show TabBar view upon successful login
-        .fullScreenCover(isPresented: $isLoggedIn) {
-            TabBar()
+//        // Show TabBar view upon successful login
+//        .fullScreenCover(isPresented: $isLoggedIn) {
+//            TabBar()
         }
     }
-}
 
 
 #Preview {
