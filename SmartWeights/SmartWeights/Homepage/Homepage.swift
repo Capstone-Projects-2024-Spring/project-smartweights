ab185af13c6d5d22580376d02dec345bff2a3a9d
dillon09776 <tup09776@temple.edu>
2024-04-17 19:07:56 -0400

    Implementing VideoCarousel view

--- SmartWeights/SmartWeights/Homepage/Homepage.swift
+++ SmartWeights/SmartWeights/Homepage/Homepage.swift
@@ -43,54 +43,11 @@ struct Homepage: View {
                 // Additional Pages Carousel
                 NavigationCarousel(buttons: buttons, iconColor: Color.africanViolet, bgColor: .black, textColor: .black)
                 
+                // Videos for video carousel
+                let bicepCurlVideo = VideoCard(videoId: "ykJmrZ5v0Oo", title: "How to Do a Dumbbell Bicep Curl", description: "Howcast")
+                
                 // Video Carousel
-                VStack {
-                    HStack {
-                        Text("Videos")
-                            .font(.title3)
-                            .padding(.top)
-                            .padding(.horizontal)
-                        Spacer()
-                        HStack {
-                            Text("See more")
-                            Image(systemName: "arrow.right")
-                                .foregroundColor(Color.africanViolet)
-                        }
-                        .padding()
-                    }
-                    ScrollView (.horizontal, showsIndicators: false) {
-                        HStack {
-                            let count = 1...4
-                            ForEach(count, id: \.self) { number in
-                                if number == 1 {
-                                    VideoCard(videoId: "ykJmrZ5v0Oo", title: "How to Do a Dumbbell Bicep Curl", description: "Howcast")
-                                } else {
-                                    VStack {
-                                        Spacer()
-                                        Image(systemName: "photo")
-                                            .foregroundStyle(Color.lightGray)
-                                        Spacer()
-                                        VStack (alignment: .leading){
-                                            Text("Video \(number)")
-                                                .font(.title3)
-                                            Text("Video Description")
-                                                .foregroundStyle(Color.lightGray)
-                                                .font(.subheadline)
-                                        }
-                                        .padding(.bottom)
-                                    }
-                                    .frame(width: 200, height: 250)
-                                    .background(Color.darkGray)
-                                    .cornerRadius(12)
-                                    .padding()
-                                }
-                            }
-                            
-                        }
-                    }
-                }
-                .foregroundStyle(.white)
-                Spacer()
+                VideoCarousel(videoCards: [bicepCurlVideo])
             }
         }
        
