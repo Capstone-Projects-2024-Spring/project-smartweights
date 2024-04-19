82ad0e040f7f7d9e1aa18005b9a487cde7ede7b5
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2024-04-18 23:40:22 -0400

    Refactored 'Achievement' struct into its own file and created a ViewModel to handle fetching data for the Profile page

---
+++ SmartWeights/SmartWeights/Achievements/Achievement.swift
@@ -0,0 +1,8 @@
+//
+//  Achievement.swift
+//  SmartWeights
+//
+//  Created by Timothy Bui on 4/18/24.
+//
+
+import Foundation
---
+++ SmartWeights/SmartWeights/Achievements/AchievementsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AchievementsViewModel.swift
+//  SmartWeights
+//
+//  Created by Timothy Bui on 4/18/24.
+//
+
+import Foundation
