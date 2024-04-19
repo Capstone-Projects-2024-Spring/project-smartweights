0827e0ebe6d47ea8037a8b00e5a296e4bfbe8636
Tu <tun42092@temple.edu>
2024-04-19 10:53:46 -0400

    create a page to show how to charge the sensor

---
+++ SmartWeights/SmartWeights/Homepage/Help/RechargeSensors.swift
@@ -0,0 +1,18 @@
+//
+//  RechargeSensors.swift
+//  SmartWeights
+//
+//  Created by Tu Ha on 4/19/24.
+//
+
+import SwiftUI
+
+struct RechargeSensors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RechargeSensors()
+}
