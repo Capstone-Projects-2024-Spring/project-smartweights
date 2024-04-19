dea8fdc755f3cdf34207bb9ab916b5f608045b51
Tu <tun42092@temple.edu>
2024-04-19 10:54:06 -0400

    added text to the page

--- SmartWeights/SmartWeights/Homepage/Help/RechargeSensors.swift
+++ SmartWeights/SmartWeights/Homepage/Help/RechargeSensors.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct RechargeSensors: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("How to Recharge Sensors")
+                    .font(.title)
+                
+                Text("Required:")
+                    .font(.headline)
+                
+                Text("• SmartWeight Sensors (Elbow and Dumbbell)")
+                
+                Text("• Micro-USB cable")
+                
+                Text("Instructions:")
+                    .font(.headline)
+                
+                Text("1. Plug the micro-USB cable into the micro-USB port on the sensor.")
+                
+                Text("2. Plug the other end of the micro-USB cable into a charging block.")
+                
+                Image("chargingPort")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                
+                Text("3. A red light will appear to indicate the sensor is charging.")
+                
+                Image("charging")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                
+                Text("4. The light will turn off when the sensor is fully charged.")
+            }
+            .padding()
+        }
+        
+        
+        
+    
     }
 }
 
