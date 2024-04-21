90cc7c5bf9edcded18c41e34076bcff9cc0cc5ed
Tu <tun42092@temple.edu>
2024-04-21 15:02:12 -0400

    fixed the color of the all feedback page

--- SmartWeights/SmartWeights/WorkoutSummaries/OverallProgress.swift
+++ SmartWeights/SmartWeights/WorkoutSummaries/OverallProgress.swift
@@ -9,137 +9,116 @@ import SwiftUI
 import CoreData
 
 
-
-
-
 class allFeedbackViewModel: ObservableObject {
     var coreDataManager: CoreDataManager
     @Published var date = Date()
-    //    @Published var shortDate: String = "" // Store short date string
     @Published var workoutSessions: [[String:Any]] = [[:]]
     @Published var WorkoutSets: [[String:Any]] = [[:]]
     var workoutNum: Int64 = 0
     
-    //    func updateShortDate() {
-    //        let formatter = DateFormatter()
-    //        formatter.dateStyle = .short
-    //        self.shortDate = formatter.string(from: date)
-    //    }
     
     init(coreDataManager: CoreDataManager) {
         self.coreDataManager = coreDataManager
         workoutSessions = coreDataManager.fetchWorkoutSessions(on: Date())
-        print(workoutSessions,"----------------------------------------INIT---------------")
         updateData(date:Date())
     }
     
     func updateData(date: Date){
         self.workoutSessions = coreDataManager.fetchWorkoutSessions(on: date)
     }
-    
-    func updateData(workoutNum: Int64){
-        self.WorkoutSets = coreDataManager.fetchExerciseSets(for: workoutNum)
-    }
 }
 
-
-//when user wants more info about that workout for that day ie. the sets
-struct moreFeedbackView: View {
-    
-    
-    var body: some View {
-        Text("Hello")
-    }
-}
-
-
-struct SelectedDateData: View {
-    @ObservedObject var viewModel: allFeedbackViewModel
-    @State var isSheetPresented: Bool = false
-    
-    
-    var body: some View{
-        Text("hello")
-    }
-    
-    
-}
 
 struct allFeedback: View {
     @ObservedObject var coreDataManager: CoreDataManager
     @ObservedObject var viewModel: allFeedbackViewModel
-    @State private var exerciseSetsText: String = ""
-    
     init(coreDataManager: CoreDataManager){
         self.coreDataManager = coreDataManager
         self.viewModel = allFeedbackViewModel(coreDataManager:coreDataManager)
     }
     
     var body: some View {
-       
-            ZStack{
-                VStack{
-                    //Title of the page
-                    HStack(alignment: .firstTextBaseline){
-                        Text("All Feedback")
-                            .font(.title)
+        
+        VStack{
+            //Title of the page
+            HStack(alignment: .firstTextBaseline){
+                Text("All Feedback")
+                    .font(.title)
+                    .bold()
+                    .fontDesign(.monospaced)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.africanViolet)
+            }
+            //calendar
+            DatePicker("Select a date", selection: $viewModel.date, displayedComponents: .date)
+                .padding()
+                .labelsHidden() // Hide the DatePicker label
+                .onChange(of: viewModel.date) { newValue in
+                    viewModel.updateData(date: newValue)
+                }
+                .onAppear {
+                    viewModel.updateData(date: viewModel.date)
+                }
+            
+            //data
+            ScrollView{
+                //get data for each workout
+                ForEach(0..<viewModel.workoutSessions.count, id: \.self) { index in
+                    VStack{
+                        let workoutSession = viewModel.workoutSessions[index]
+                        Text("Overall Workout Session \(index+1)")
+                            .font(.title2)
                             .bold()
-                            .fontDesign(.monospaced)
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(.africanViolet)
-                    }
-                    
-                    DatePicker("Select a date", selection: $viewModel.date, displayedComponents: .date)
-                        .padding()
-                        .labelsHidden() // Hide the DatePicker label
-                        .onChange(of: viewModel.date) { newValue in
-                            viewModel.updateData(date: newValue)
+                            .padding(.top, 30)
+                            .foregroundColor(.white)
+                            .underline()
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Curl Acceleration - \(Int(workoutSession["overallCurlAcceleration"] as? Double ?? 0.0))%")
+                            Text("Wrist Stability (Left Right) - \(Int(workoutSession["overallWristStabilityLeftRight"] as? Double ?? 0.0))%")
+                            Text("Wrist Stability (Up Down) - \(Int(workoutSession["overallWristStabilityUpDown"] as? Double ?? 0.0))%")
+                            Text("Elbow Swing - \(Int(workoutSession["overallElbowSwing"] as? Double ?? 0.0))%")
+                            Text("Elbow Flare(Left Right) - \(Int(workoutSession["overallElbowFlareLeftRight"] as? Double ?? 0.0))%")
+                            Text("Elbow Flare (Up Down) - \(Int(workoutSession["overallElbowFlareUpDown"] as? Double ?? 0.0))%")
                         }
-                        .onAppear {
-                            viewModel.updateData(date: viewModel.date)
-                        }
-                    ScrollView{
-                        ForEach(0..<viewModel.workoutSessions.count, id: \.self) { index in
-                            let workoutSession = viewModel.workoutSessions[index]
-                            Text("Workout Session \(index+1):")
-                                .font(.title2)
-                                .bold()
-                                .padding(.top, 30)
-                                .foregroundColor(.africanViolet)
-                            VStack(alignment: .leading, spacing: 10) {
-                                Text("Overall Curl Acceleration - \(workoutSession["overallCurlAcceleration"] ?? 0)%")
-                                Text("Overall Wrist Stability (Left Right) - \(workoutSession["overallWristStabilityLeftRight"] ?? 0)%")
-                                Text("Overall Wrist Stability (Up Down) - \(workoutSession["overallWristStabilityUpDown"] ?? 0)%")
-                                Text("Overall Elbow Swing - \(workoutSession["overallElbowSwing"] ?? 0)%")
-                                Text("Overall Elbow Flare(Left Right) - \(workoutSession["overallElbowFlareLeftRight"] ?? 0)%")
-                                Text("Overall Elbow Flare (Up Down) - \(workoutSession["overallElbowFlareUpDown"] ?? 0)%")
-                            }
-                            .padding(.horizontal)
-                            let exerciseSets = viewModel.coreDataManager.fetchExerciseSets(for:workoutSession["workoutNum"] as? Int64 ?? 0)
-                            ForEach(0..<exerciseSets.count, id: \.self) { index in
-                                let exerciseSet = exerciseSets[index]
-                                Text(" Set \(index+1):")
-                                    .font(.headline)
-                                    .padding(.top, 20)
-                                    .foregroundColor(.africanViolet)
+                        .foregroundColor(.white)
+                        //get data for each set in the workout
+                        let exerciseSets = viewModel.coreDataManager.fetchExerciseSets(for:workoutSession["workoutNum"] as? Int64 ?? 0)
+                        ForEach(0..<exerciseSets.count, id: \.self) { index in
+                            let exerciseSet = exerciseSets[index]
+                            DisclosureGroup(content: {
                                 VStack(alignment: .leading, spacing: 10) {
-                                    Text("Curl Acceleration - \(exerciseSet["avgCurlAcceleration"] as? Int ?? 0)%")
-                                    Text("Wrist Stability (Left Right) - \(exerciseSet["avgWristStabilityLeftRight"] as? Int ?? 0)%")
-                                    Text("Wrist Stability (Up Down) - \(exerciseSet["avgWristStabilityUpDown"] as? Int ?? 0)%")
-                                    Text("Elbow Swing - \(exerciseSet["avgElbowSwing"] as? Int ?? 0)")
-                                    Text("Elbow Flare(Left Right) - \(exerciseSet["avgElbowFlareLeftRight"] as? Int ?? 0)%")
-                                    Text("Elbow Flare (Up Down) - \(exerciseSet["avgElbowFlareUpDown"] as? Int ?? 0)%")
+                                    Text("Curl Acceleration - \(Int(exerciseSet["avgCurlAcceleration"] as? Double ?? 0))%")
+                                    Text("Wrist Stability (Left Right) - \(Int(exerciseSet["avgWristStabilityLeftRight"] as? Double ?? 0))%")
+                                    Text("Wrist Stability (Up Down) - \(Int(exerciseSet["avgWristStabilityUpDown"] as? Double ?? 0))%")
+                                    Text("Elbow Swing - \(Int(exerciseSet["avgElbowSwing"] as? Double ?? 0))%")
+                                    Text("Elbow Flare(Left Right) - \(Int(exerciseSet["avgElbowFlareLeftRight"] as? Double ?? 0))%")
+                                    Text("Elbow Flare (Up Down) - \(Int(exerciseSet["avgElbowFlareUpDown"] as? Double ?? 0))%")
                                 }
                                 .padding(.horizontal)
-                            }
+                                .foregroundColor(.white)
+                                .font(.callout)
+                                .padding(.bottom, 10)
+                            }, label: {
+                                HStack {
+                                    Text(" Set \(index+1)")
+                                        .font(.title3)
+                                        .foregroundColor(.white)
+                                        .bold()
+                                        .underline()
+                                }
+                                .frame(maxWidth: .infinity, alignment: .center)
+                            })
                         }
+                        
                     }
-                    
-                    
-                    
-                    
-                
+                    .padding(.horizontal)
+                    .frame(width: 350)
+                    .background(Color.africanViolet)
+                    .cornerRadius(20)
+                }
             }
+            
+            
         }
     }
 }
