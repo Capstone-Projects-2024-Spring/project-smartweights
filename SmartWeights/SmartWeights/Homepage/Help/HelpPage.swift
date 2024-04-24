62cbef695bbf42b1644755a7323b1245e077a321
1dillon <dyoungshi004@gmail.com>
2024-04-24 10:16:50 -0400

    Added Homepage help page

--- SmartWeights/SmartWeights/Homepage/Help/HelpPage.swift
+++ SmartWeights/SmartWeights/Homepage/Help/HelpPage.swift
@@ -10,15 +10,37 @@ import SwiftUI
 struct HelpPage: View {
     var body: some View {
         
-        var numSections = 9
-        
-        NavigationSplitView {
+        NavigationStack {
             List {
+                
+                // Login
                 NavigationLink {
-                    Text("Hello World")
+                    VStack (alignment: .leading){
+                        Text("Login")
+                            .font(.title)
+                            .padding(.bottom    )
+                        Text("When you first open the application, you will be greeted with our sign in page. Once you sign in using your apple account, you will automatically be signed in the app in the future.")
+                        Spacer()
+                    }
+                    .padding(.horizontal)
                 } label: {
                     Text("Login")
                 }
+                
+                // Home page
+                NavigationLink {
+                    VStack (alignment: .leading){
+                        Text("Homepage")
+                            .font(.title)
+                            .padding(.bottom)
+                        Text("After login, you will be greeted with the home page. The home page contains a direct path to start a workout and other shortcuts for your convenience. This page also contains educational videos to improve your SmartWeights experience. This tutorial can be viewed anytime from this page.")
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                } label: {
+                    Text("Homepage")
+                }
+                
             }
             .navigationTitle("Help")
 
@@ -30,8 +52,6 @@ struct HelpPage: View {
 //                Text("Selected row \($0)")
 //            }
 //            .navigationTitle("Split View")
-        } detail: {
-            Text("Please select a row")
         }
         
     }
