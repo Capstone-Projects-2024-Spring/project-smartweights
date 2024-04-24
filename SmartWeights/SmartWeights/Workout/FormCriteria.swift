e0ecd533dc4c1f4f8109e4e2434786c9cdcf10c7
Tu <tun42092@temple.edu>
2024-04-24 09:38:35 -0400

    added api comments to FormCriteria

--- SmartWeights/SmartWeights/Workout/FormCriteria.swift
+++ SmartWeights/SmartWeights/Workout/FormCriteria.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-
+/// `FormCriteria` is a class that contains the criteria for determining the form of the user during a workout.
 class FormCriteria: ObservableObject{
     
     //dumbbell averages
@@ -33,7 +33,19 @@ class FormCriteria: ObservableObject{
         let randomIndex = Int.random(in: 0..<goodFormPhrases.count)
         return goodFormPhrases[randomIndex]
     }
-    
+    /// `resetListofData` is a function that clears the data for the next workout.
+    ///
+    /// This function removes all elements from the following lists:
+    /// - `listOfDumbbellAverage`
+    /// - `listOfWristUpDownAverage`
+    /// - `listOfWristLeftRightAverage`
+    /// - `listOfElbowSwingAverage`
+    /// - `listOfElbowFlareUpDownAverage`
+    /// - `listOfElbowFlareForwardBackAverage`
+    ///
+    /// After clearing the data, it prints a message to the console.
+    ///
+    /// - Note: This function should be called before starting a new workout to ensure that the data from the previous workout does not affect the new one.
     func resetListofData(){
         listOfDumbbellAverage.removeAll()
         listOfWristUpDownAverage.removeAll()
@@ -66,7 +78,12 @@ class FormCriteria: ObservableObject{
         "overallWorkoutElbowFlareForwardBackward": 0.0
     ]
     
-    
+    /// `UpdateWorkoutAnalysis` is a function that updates the workout analysis based on the data collected during the workout.
+    /// - Parameters:
+    ///   - totalSets:Int
+    ///   - dumbbellArray: 
+    ///   - elbowArray: 
+    /// - Returns: 
     func UpdateWorkoutAnalysis(totalSets:Int,dumbbellArray: [[Int]],elbowArray: [[Int]]) -> [String:Double]{
         var workoutAnalysis: [String:Double] = [:]
         workoutAnalysis["averageUpDownAcceleration"] = averageUpDownAcceleration(array: dumbbellArray, append: true) * 100
@@ -77,7 +94,7 @@ class FormCriteria: ObservableObject{
         workoutAnalysis["overallDumbbellTwistingLeftRight"] = overallDumbbellTwisting(totalSets: totalSets).1 * 100
         workoutAnalysis["averageElbowSwing"] = averageElbowSwing(array: elbowArray, append: true) * 100
         workoutAnalysis["averageElbowFlareUpDown"] = averageElbowFlareUpDown(array: elbowArray) * 100
-        workoutAnalysis["averageElbowFlareForwardBackward"] = averageElbowFlareFowardBackward(array: elbowArray) * 100
+        workoutAnalysis["averageElbowFlareForwardBackward"] = averageElbowFlareForwardBackward(array: elbowArray) * 100
         workoutAnalysis["overallWorkoutElbowSwing"] = overallWorkoutElbowSwing(totalSets: totalSets) * 100
         workoutAnalysis["overallWorkoutElbowFlareUpDown"] = overallWorkoutElbowFlare(totalSets: totalSets).0 * 100
         workoutAnalysis["overallWorkoutElbowFlareForwardBackward"] = overallWorkoutElbowFlare(totalSets: totalSets).1 * 100
@@ -90,6 +107,13 @@ class FormCriteria: ObservableObject{
     
     //---------------------DUMBBELL-------------------//
     
+
+    /// `averageUpDownAcceleration` is a function that calculates the average up and down acceleration of the dumbbell during a set.
+    /// - Parameters:
+    ///  - array: An array of arrays containing the x, y, and z acceleration data of the dumbbell.
+    /// - append: A boolean value that determines whether to append the data to the list of dumbbell averages.
+    /// - Returns: The percentage of good form for the up and down acceleration of the dumbbell.
+
     //read the Z axis rotation and gives an average of the up and down for that set
     func averageUpDownAcceleration(array: [[Int]],append: Bool) -> Double {
         /*
@@ -124,8 +148,12 @@ class FormCriteria: ObservableObject{
         return percentage
     }
     
+    /// `averageWristLeftRightRotation` is a function that calculates the average left and right rotation of the wrist during a set.
+    /// - Parameters:
+    /// - array: An array of arrays containing the x, y, and z rotation data of the wrist.
+    /// - Returns: The percentage of good form for the left and right rotation of the wrist.
     
-    //twisting writst left and right
+    //twisting wrist left and right
     func averageWristLeftRightRotation(array: [[Int]]) -> Double {
         //measurement from the X rotation
         
@@ -154,6 +182,11 @@ class FormCriteria: ObservableObject{
         
         return percentage
     }
+
+    /// `averageWristUpDownRotation` is a function that calculates the average up and down rotation of the wrist during a set.
+    /// - Parameters:
+    /// - array: An array of arrays containing the x, y, and z rotation data of the wrist.
+    /// - Returns: The percentage of good form for the up and down rotation of the wrist.
     
     //twisting up and down
     func averageWristUpDownRotation(array: [[Int]]) -> Double {
@@ -186,7 +219,11 @@ class FormCriteria: ObservableObject{
     }
     
     
-    
+    /// `overallWorkoutUpDownAverage` is a function that calculates the overall average of the up and down acceleration of the dumbbell for the entire workout.
+    /// - Parameters:
+    /// - totalSets: The total number of sets in the workout.
+    /// - Returns: The percentage of good form for the up and down acceleration of the dumbbell for the entire workout.
+
     //get the average of each set and compute the overall average for the workout
     //(sum of averages from sets)/total sets
     func overallWorkoutUpDownAverage(totalSets: Int) -> Double {
@@ -205,7 +242,9 @@ class FormCriteria: ObservableObject{
     }
     
     
-    
+    /// `overallDumbbellTwisting` is a function that calculates the overall average of the left and right and up and down rotation of the wrist for the entire workout.
+    /// - Parameter totalSets: 
+    /// - Returns: A tuple containing the average up and down rotation of the wrist and the average left and right rotation of the wrist for the entire workout.
     func overallDumbbellTwisting(totalSets: Int) -> (Double, Double) {
         guard totalSets != 0 else {
             return (1.0, 1.0)
@@ -230,7 +269,12 @@ class FormCriteria: ObservableObject{
     
     //--------------------------------ELBOW-------------------------------//
     
-    //measures if the eblow is swinging back and forth too much
+    /// `averageElbowSwing` is a function that calculates the average elbow swing of the user during a set.
+    /// - Parameters: 
+    /// - array: An array of arrays containing the x, y, and z rotation data of the elbow.
+    /// - append: A boolean value that determines whether to append the data to the list of elbow averages.
+    /// - Returns: The percentage of good form for the elbow swing.
+    //measures if the elbow is swinging back and forth too much
     //use MPU6050-2
     func averageElbowSwing(array: [[Int]], append: Bool) -> Double {
         var count = 0 //total data collected
@@ -261,7 +305,10 @@ class FormCriteria: ObservableObject{
         return percentage
     }
     
-    
+    /// `averageElbowFlareUpDown` is a function that calculates the average elbow flare of the user during a set.
+    /// - Parameters:
+    /// - array: An array of arrays containing the x, y, and z rotation data of the elbow.
+    /// - Returns: The percentage of good form for the elbow flare.
     //flaring elbow up and down
     func averageElbowFlareUpDown(array: [[Int]]) -> Double {
         var count = 0 //total data collected
@@ -289,9 +336,12 @@ class FormCriteria: ObservableObject{
     }
     
     
-    
+    /// `averageElbowFlareForwardBackward` is a function that calculates the average elbow flare of the user during a set.
+    /// - Parameters:
+    /// - array: An array of arrays containing the x, y, and z rotation data of the elbow.
+    /// - Returns: The percentage of good form for the elbow flare.
     //flaring elbow forward and black
-    func averageElbowFlareFowardBackward(array: [[Int]]) -> Double {
+    func averageElbowFlareForwardBackward(array: [[Int]]) -> Double {
         var count = 0 //total data collected
         var good = 0 //data in range of good form
         var percentage: Double = 1 //return
@@ -316,7 +366,10 @@ class FormCriteria: ObservableObject{
         return percentage
     }
     
-    
+    /// `overallWorkoutElbowSwing` is a function that calculates the overall average of the elbow swing for the entire workout.
+    /// - Parameters:
+    /// - totalSets: The total number of sets in the workout.
+    /// - Returns: The percentage of good form for the elbow swing for the entire workout.
     
     //get average of elbow swinging for the workout
     
@@ -336,6 +389,10 @@ class FormCriteria: ObservableObject{
         return percentage
     }
     
+    /// `overallWorkoutElbowFlare` is a function that calculates the overall average of the elbow flare for the entire workout.
+    /// - Parameters:
+    /// - totalSets: The total number of sets in the workout.
+    /// - Returns: A tuple containing the average up and down elbow flare and the average forward and backward elbow flare for the entire workout.
     
     //gives the overall elbow flaring for the whole workout
     func overallWorkoutElbowFlare(totalSets: Int) -> (Double, Double) {
@@ -366,6 +423,12 @@ class FormCriteria: ObservableObject{
     
     //----------------DANGEROUS MOVEMENTS----------------//
     
+    /// `dangerousForm` is a function that checks if the user is moving too fast and is in a dangerous position.
+    /// - Parameters:
+    /// - dumbbellData: An array containing the x, y, and z acceleration data of the dumbbell.
+    /// - elbowData: An array containing the x, y, and z rotation data of the elbow.
+    /// - Returns: A boolean value indicating whether the user is in a dangerous position.
+
     //Threshold if they are moving too fast and is dangerous
     //will be ran in a while loop to continuously check the user
     func dangerousForm(dumbbellData:[Int], elbowData: [Int]) -> Bool{
@@ -377,6 +440,13 @@ class FormCriteria: ObservableObject{
     }
     
     //--------------FEEDBACK---------------------//
+
+    /// `giveFeedback` is a function that provides feedback to the user based on the data collected during the workout.
+    /// - Parameters:
+    /// - dumbbellArray: An array of arrays containing the x, y, and z acceleration data of the dumbbell.
+    /// - elbowArray: An array of arrays containing the x, y, and z rotation data of the elbow.
+    /// - Returns: A tuple of strings containing the feedback for the user.
+
     
     //returns a tuple of strings for the specific feedback
     func giveFeedback(dumbbellArray: [[Int]],elbowArray: [[Int]] ) -> (String,String,String,String){
