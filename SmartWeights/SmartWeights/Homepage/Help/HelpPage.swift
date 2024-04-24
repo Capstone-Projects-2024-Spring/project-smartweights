0990860d2c397ed25bccedd3d2375b7bf3cc635c
1dillon <dyoungshi004@gmail.com>
2024-04-24 09:22:58 -0400

    Added help/instruction manual page

---
+++ SmartWeights/SmartWeights/Homepage/Help/HelpPage.swift
@@ -0,0 +1,18 @@
+//
+//  HelpPage.swift
+//  SmartWeights
+//
+//  Created by Dillon Shi on 4/24/24.
+//
+
+import SwiftUI
+
+struct HelpPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HelpPage()
+}
