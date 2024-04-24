6433692f6735323e1e0da42b2ed700171fb76614
1dillon <dyoungshi004@gmail.com>
2024-04-24 10:15:57 -0400

    Creating a nNavigationStack

--- SmartWeights/SmartWeights/Homepage/Help/HelpPage.swift
+++ SmartWeights/SmartWeights/Homepage/Help/HelpPage.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct HelpPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        var numSections = 9
+        
+        NavigationSplitView {
+            List {
+                NavigationLink {
+                    Text("Hello World")
+                } label: {
+                    Text("Login")
+                }
+            }
+            .navigationTitle("Help")
+
+            
+//            List(0..<numSections, id: \.self) { index in
+//                NavigationLink("Row \(index)", value: index)
+//            }
+//            .navigationDestination(for: Int.self) {
+//                Text("Selected row \($0)")
+//            }
+//            .navigationTitle("Split View")
+        } detail: {
+            Text("Please select a row")
+        }
+        
     }
 }
 
