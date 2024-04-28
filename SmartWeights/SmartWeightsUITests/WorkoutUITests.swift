ad21b7cf1c0f9c5c8e133a9ede15fb0be9dab700
parchea <parodamchea@gmail.com>
2024-04-28 17:56:13 -0400

    revert back

--- SmartWeights/SmartWeightsUITests/WorkoutUITests.swift
+++ SmartWeights/SmartWeightsUITests/WorkoutUITests.swift
@@ -2,13 +2,12 @@
 //  WorkoutUITests.swift
 //  SmartWeightsUITests
 //
-//  Created by Par Chea on 04/27/24.
+//  Created by Tu Ha on 2/29/24.
 //
-
 import XCTest
-@testable import SmartWeights
 
-class WorkoutMainPageTests: XCTestCase{
+final class WorkoutUITests: XCTestCase {
+    
     
     var app: XCUIApplication!
     
@@ -18,98 +17,58 @@ class WorkoutMainPageTests: XCTestCase{
         app.launch()
     }
     
-    func testWorkoutStartsCorrectly() {
-        // Assume that the Start Workout button has an accessibility label "StartWorkoutButton"
-        let startWorkoutButton = app.buttons["StartWorkoutButton"]
-        startWorkoutButton.tap()
-        
-        // Interact with the Sets text field
-        let setsField = app.textFields[ "Sets"]
-        setsField.tap()
-        setsField.typeText("1") // Enter a valid number of sets
-        
-        // Interact with the Reps text field
-        let repsField = app.textFields[ "Reps"]
-        repsField.tap()
-        repsField.typeText("1") // Enter a valid number of reps
-        
-        // Interact with the Weight text field
-        let lbsField = app.textFields["Weight (lbs)"]
-        lbsField.tap()
-        lbsField.typeText("1") // Enter a valid weight in pounds
-        
-        
-        let countdownTimer = app.textFields["Count down Timer (s)"]
-        countdownTimer.tap()
-        countdownTimer.typeText("5")
-
-        // Tap the Start Workout button to submit details and start the workout
-        let workoutDetailsStartButton = app.buttons["Start Workout"] // Assuming this is the button's identifier within the modal/pop-up
-        workoutDetailsStartButton.tap()
-        Thread.sleep(forTimeInterval: 8.0) // Sleeps the current thread for 5 seconds
-        
-        // Check if the motivational phrase has updated
-        XCTAssert(app.staticTexts["First set, let's go!"].exists)
+    override func tearDownWithError() throws {
+        // Clean up any resources if needed
     }
     
-    func testWorkoutInputInvalidate() {
-        app.buttons["StartWorkoutButton"].tap()
-        
-        app.sheets.buttons["Enter Workout Details"].tap()
-        // Assuming you have text fields with placeholders for workout details
-        let setsField = app.textFields["Sets"]
-        setsField.tap()
-        setsField.typeText("abc") // Invalid input
-        
-        let startButton = app.buttons["Start Workout"]
-        startButton.tap()
-        // Check for an alert indicating invalid input
-        XCTAssert(app.alerts["Invalid Input"].exists)
+    
+    
+    
+    //WORKOUT MAIN PAGE
+    
+    func testDatePicker() throws {
+        //        let datePicker = app.datePickers["WorkoutDatePicker"]
+        //
+        //                // When: Select a date programmatically
+        //                let selectedDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())! // Set the desired date
+        //                datePicker.setDate(selectedDate)
+        //
+        //                // Then: Assert that the selected date is correct
+        //                XCTAssertEqual(datePicker.date, selectedDate)
     }
     
-    func testWorkoutInputValidation(){
-        let startWorkoutButton = app.buttons["StartWorkoutButton"]
-        startWorkoutButton.tap()
-        
-        // Interact with the Sets text field
-        let setsField = app.textFields["Sets"]
-        setsField.tap()
-        setsField.typeText("1") // Enter a valid number of sets
-        
-        // Interact with the Reps text field
-        let repsField = app.textFields[ "Reps"]
-        repsField.tap()
-        repsField.typeText("1") // Enter a valid number of reps
-        
-        // Interact with the Weight text field
-        let lbsField = app.textFields[ "Weight (lbs) "]
-        lbsField.tap()
-        lbsField.typeText("1") // Enter a valid weight in pounds
-        
-        let countdownTimer = app.textFields["Count down Timer (s)"]
-        countdownTimer.tap()
-        countdownTimer.typeText("5")
-        
-        // Tap the Start Workout button to submit details and start the workout
-        let workoutDetailsStartButton =
-        app.buttons["Start Workout"] // Assuming this is the button's identifier within the modal/pop-up
-        workoutDetailsStartButton.tap()
-        
-        Thread.sleep(forTimeInterval: 8.0) // Sleeps the current thread for 5 seconds
-        // Assuming FinishSetButton is available after submitting the workout details and starting the workout
-        
-        let finishSetButton = app.buttons["FinishSetButton" ]
-        finishSetButton.tap()
-    }
     func testMicWorkoutButton(){
-        let startWorkoutButton = app.buttons["StartWorkoutButton"]
-        startWorkoutButton.tap()
-        let micWorkout = app.buttons["Start Voice Command"]
+        
+        let micWorkout = app.buttons["micWorkoutButton"]
         micWorkout.tap()
+        
         XCTAssertTrue(micWorkout.exists)
     }
     
-    override func tearDownWithError() throws {
-        app = nil
+    func testStartWorkoutButton(){
+        let micWorkout = app.buttons["micWorkoutButton"]
+        micWorkout.tap()
+        let startWorkout = app.buttons["startWorkoutButton"]
+        startWorkout.tap()
+        
+        XCTAssertTrue(startWorkout.exists)
     }
+    
+    func testEndWorkoutButton(){
+        let micWorkout = app.buttons["micWorkoutButton"]
+        micWorkout.tap()
+        let endWorkout = app.buttons["endWorkoutButton"]
+        endWorkout.tap()
+        
+        XCTAssertTrue(endWorkout.exists)
+    }
+    
+    
+    
+    
+    
+    
+    //WORKOUT PROGRESS PAGE
+    
+    
 }
